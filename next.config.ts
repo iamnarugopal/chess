4f6bea3245d2e9bdca349eb486df1bc49f074e3b
iamnarugopal <veeruk0217@gmail.com>
2026-07-16 13:24:46 +0530

    refactor: remove unnecessary config options from nextConfig

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
   distDir: "build",
-  output: "export",
 };
 
 export default nextConfig;
